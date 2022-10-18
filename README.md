355e339ef7cea8a4509f922015efabee32527bd0
hadiah <hadiah.almalki@gmail.com>
2022-10-18 13:31:31 +0000

    library

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# npm-library
+# npm-library 
+
+npm library that it converts dash/underscore delimited words into camel casing.(known as Upper Camel Case, also often referred to as Pascal case).
---
+++ index.js
@@ -0,0 +1,21 @@
+function toCamelCase(str) {
+    let strArray;
+
+    if (str === "") {
+        return "";
+    }
+    if (str.indexOf("-") !== -1) {
+        strArray = str.split("-")
+    } else {
+        strArray = str.split("_")
+    }
+    let convertString = strArray[0];
+
+    for (let i = 1; i < strArray.length; i++) {
+        convertString += capitalize(strArray[i]);
+    }
+    return convertString;
+}
+let capitalize = (str) => {
+    return str[0].toUpperCase() + str.slice(1);
+}
